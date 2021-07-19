9093b8d3f4505917ee577d94609a1f1a80882d54
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-07-19 14:51:49 +0200

    feat: Provide most basic approach for gauges (#6)

---
+++ src/Promitor.Parsers.Prometheus.Core/Models/MetricType.cs
@@ -0,0 +1,11 @@
+﻿namespace Promitor.Parsers.Prometheus.Core.Models
+{
+    public enum MetricTypes
+    {
+        NotSpecified,
+        Counter,
+        Gauge,
+        Histogram,
+        Summary
+    }
+}
--- src/Promitor.Parsers.Prometheus.Core/PrometheusMetricsParser.cs
+++ src/Promitor.Parsers.Prometheus.Core/PrometheusMetricsParser.cs
@@ -1,12 +1,161 @@
-﻿using System.Collections.Generic;
+﻿using Promitor.Parsers.Prometheus.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Promitor.Parsers.Prometheus.Core
 {
     public class PrometheusMetricsParser
     {
-        public static List<object> Parse(string rawMetrics)
+        const string MetricInfoRegex = @"# (\w+) (\w*) (.*)";
+        const string MeasurementRegex = @"(.+){(.*)} (-?\d+(?:\.\d*)*) (\d*)";
+
+        public static async Task<List<Gauge>> ParseAsync(Stream rawMetricsStream)
         {
-            return new List<object>();
+            var originalPosition = rawMetricsStream.Position;
+
+            // Go to the beginning of the stream
+            if(rawMetricsStream.CanSeek)
+            {
+                rawMetricsStream.Seek(0, SeekOrigin.Begin);
+            }
+
+            // Interpret the metrics
+            var metrics = await InterpretRawMetricsStreamAsync(rawMetricsStream);
+
+            // Reset stream to original position
+            if(rawMetricsStream.CanSeek)
+            {
+                rawMetricsStream.Seek(originalPosition, SeekOrigin.Begin);
+            }
+
+            return metrics;
+        }
+
+        private static async Task<List<Gauge>> InterpretRawMetricsStreamAsync(Stream rawMetricsStream)
+        {
+            var metrics = new List<Gauge>();
+            var streamReader = new StreamReader(rawMetricsStream);
+            
+            Gauge unfinishedGauge = null;
+            MetricTypes currentMetricType = MetricTypes.NotSpecified;
+            var wasReadingMeasurements = false;
+            
+            var line = await streamReader.ReadLineAsync();
+            while(rawMetricsStream.Position < rawMetricsStream.Length)
+            {
+                if(line.StartsWith("#"))
+                {
+                    if(wasReadingMeasurements == true)
+                    {
+                        // We're done, starting to interpret the next gauge
+                        metrics.Add(unfinishedGauge);
+                        unfinishedGauge = null;
+                    }
+
+                    if(unfinishedGauge == null)
+                    {
+                        unfinishedGauge = new Gauge();
+                    }
+
+                    var metricInfoMatch = Regex.Match(line, MetricInfoRegex);
+                    if (metricInfoMatch.Success == false)
+                    {
+                        throw new Exception("Unable to parse the metric context information");
+                    }
+
+                    var scenario = metricInfoMatch.Groups[1].Value;
+                    switch(scenario)
+                    {
+                        case "HELP":
+                            unfinishedGauge.Description = metricInfoMatch.Groups[3].Value;
+                            break;
+                        case "TYPE":
+                            unfinishedGauge.Name = metricInfoMatch.Groups[2].Value;
+                            currentMetricType = Enum.Parse<MetricTypes>(metricInfoMatch.Groups[3].Value,ignoreCase: true);
+                            break;
+                    }
+                    
+                    wasReadingMeasurements = false;
+                }
+                else
+                {
+                    if(currentMetricType != MetricTypes.Gauge)
+                    {
+                        Console.WriteLine("Current metric type is not a gauge, ignoring.");
+                    }
+                    else
+                    {
+                        GaugeMeasurement measurement = ParseMeasurement(line);
+                        unfinishedGauge.Measurements.Add(measurement);
+                    }
+
+                    wasReadingMeasurements = true;
+                }
+
+                line = await streamReader.ReadLineAsync();
+            }
+
+            return metrics;
+        }
+
+        private static GaugeMeasurement ParseMeasurement(string line)
+        {
+            var measurement = new GaugeMeasurement();
+            var regexOutcome = Regex.Match(line, MeasurementRegex);
+            if (regexOutcome.Success == false)
+            {
+                throw new Exception($"Measurement doesn't follow the required Regex statement ({MeasurementRegex})");
+            }
+
+            // Assign value
+            measurement.Value = ParseMetricValue(regexOutcome);
+
+            // Get all contextual information
+            ParseMetricLabels(regexOutcome, measurement);
+
+            // Assign time, if available
+            measurement.Timestamp = ParseMetricTimestamp(regexOutcome);
+
+            return measurement;
+        }
+
+        private static double ParseMetricValue(Match regexOutcome)
+        {
+            if (regexOutcome.Groups.Count < 4 || regexOutcome.Groups[3].Value == null)
+            {
+                throw new Exception("No metric value was found");
+            }
+
+            return double.Parse(regexOutcome.Groups[3].Value);
+        }
+
+        private static DateTimeOffset? ParseMetricTimestamp(Match regexOutcome)
+        {            
+            if (regexOutcome.Groups.Count < 5 || regexOutcome.Groups[4].Value == null)
+            {
+                return null;
+            }
+
+            var unixTimeInSeconds = long.Parse(regexOutcome.Groups[4].Value);
+            return DateTimeOffset.FromUnixTimeMilliseconds(unixTimeInSeconds);
+        }
+
+        private static void ParseMetricLabels(Match regexOutcome, GaugeMeasurement measurement)
+        {
+            var labels = regexOutcome.Groups[2].Value;
+            
+            // Get every individual raw label
+            foreach (var rawLabel in labels.Split(','))
+            {
+                // Split label into information
+                var splitLabelInfo = rawLabel.Split('=');
+
+                // Add to the outcome
+                measurement.Labels.Add(splitLabelInfo[0], splitLabelInfo[1].Replace("\"", ""));
+            }
         }
     }
 }
