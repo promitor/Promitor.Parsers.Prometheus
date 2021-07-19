b363c20a51d8b5aa2daefaa8e7b189201e63b48b
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-07-19 10:44:51 +0200

    Basic solution
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Promitor.Parsers.Prometheus.Core/PrometheusMetricsParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Promitor.Parsers.Prometheus.Core
+{
+    public class PrometheusMetricsParser
+    {
+        public static List<object> Parse(string rawMetrics)
+        {
+            return new List<object>();
+        }
+    }
+}
---
+++ src/Promitor.Parsers.Prometheus.Tests/PrometheusMetricsParserTests.cs
@@ -0,0 +1,26 @@
+﻿using Promitor.Parsers.Prometheus.Core;
+using System;
+using System.ComponentModel;
+using System.IO;
+using Xunit;
+
+namespace Promitor.Parsers.Prometheus.Tests
+{
+    [Category("Unit")]
+    public class PrometheusMetricsParserTests
+    {
+        [Fact]
+        public void Parse_ValidInput_ReturnsMetrics()
+        {
+            // Arrange
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Samples", "raw-metrics.txt");
+            var rawMetrics = File.ReadAllText(filePath);
+
+            // Act
+            var metrics = PrometheusMetricsParser.Parse(rawMetrics);
+
+            // Assert
+            Assert.NotNull(metrics);
+        }
+    }
+}
