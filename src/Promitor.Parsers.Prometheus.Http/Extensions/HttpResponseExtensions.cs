a904dbf8cd9dffd412e8db2d2c4471f68d7cfdd2
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-07-20 10:23:19 +0200

    feat: Provide HTTP extension to scrape metrics (#9)

---
+++ src/Promitor.Parsers.Prometheus.Http/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Promitor.Parsers.Prometheus.Core;
+using Promitor.Parsers.Prometheus.Core.Models.Interfaces;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpResponseExtensions
+    {
+        /// <summary>
+        /// Reads & parses an HTTP response as Prometheus metrics
+        /// </summary>
+        /// <param name="httpResponse">Http response message to parse</param>
+        /// <returns>List of Prometheus metrics</returns>
+        public static async Task<List<IMetric>> ReadAsPrometheusMetricsAsync(this HttpResponse httpResponse)
+        {
+            if(httpResponse == null || httpResponse.Body == null)
+            {
+                return Enumerable.Empty<IMetric>().ToList();
+            }
+
+            var metrics = await PrometheusMetricsParser.ParseAsync(httpResponse.Body);
+            return metrics;
+        }
+    }
+}
---
+++ src/Promitor.Parsers.Prometheus.Http/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Promitor.Parsers.Prometheus.Core;
+using Promitor.Parsers.Prometheus.Core.Models.Interfaces;
+
+namespace System.Net.Http
+{
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Reads & parses an HTTP response as Prometheus metrics
+        /// </summary>
+        /// <param name="httpResponseMessage">Http response message to parse</param>
+        /// <returns>List of Prometheus metrics</returns>
+        public static async Task<List<IMetric>> ReadAsPrometheusMetricsAsync(this HttpResponseMessage httpResponseMessage)
+        {
+            if(httpResponseMessage == null || httpResponseMessage.Content == null)
+            {
+                return Enumerable.Empty<IMetric>().ToList();
+            }
+
+            var responseStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+            var metrics = await PrometheusMetricsParser.ParseAsync(responseStream);
+            return metrics;
+        }
+    }
+}
